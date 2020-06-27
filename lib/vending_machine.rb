0a90e988f1762872c6122c0b460a37110b61c4ac
ima1zumi <tesioa@gmail.com>
2020-06-27 22:21:15 +0900

    step1: ジュースの管理を実装

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
+require './lib/drink'
+
 class VendingMachine
+  def initialize
+    @stocks = []
+    5.times do
+      @stocks << Drink.cola
+    end
+  end
+
+  def current_stocks
+    drink = @stocks.first
+    [{name: drink.name, price: drink.price, stock: @stocks.size}]
+  end
 end
