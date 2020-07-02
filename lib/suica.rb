4441468ad2cd9c18b0b133f7574057ebc3ab1312
ima1zumi <tesioa@gmail.com>
2020-07-02 18:01:32 +0900

    step0: Suicaのチャージ機能を実装

--- lib/suica.rb
+++ lib/suica.rb
@@ -1,2 +1,11 @@
 class Suica
+  attr_accessor :balance
+
+  def initialize(money)
+    @balance = money
+  end
+
+  def deposit(money)
+    @balance = money >= 100 ? @balance + money : @balance
+  end
 end
